418aa2334864147afca677295a8db69c9b107f58
infected-by-js <infectedbyjs@gmail.com>
2022-04-07 03:11:09 +1000

    change browserRouter to hashRouter

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import {HOME_PAGE_ROUTE, PRODUCT_PAGE_ROUTE} from './routes';
 import {HomePage} from '../pages/home/HomePage';
 import {ProductPage} from '../pages/product/ProductPage';
@@ -6,12 +6,12 @@ import {NotFoundPage} from '../pages/not-found/NotFoundPage';
 
 export const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/">
       <Routes>
         <Route path={HOME_PAGE_ROUTE} element={<HomePage />} />
         <Route path={PRODUCT_PAGE_ROUTE} element={<ProductPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
